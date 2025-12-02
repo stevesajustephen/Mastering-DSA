95442a1eda48c38ce1664f7c6f924674cdf2943e
Steve Saju <stevesajustephen@gmail.com>
2025-12-02 08:49:08 +0100

    anagram update

--- anagram.js
+++ anagram.js
@@ -1,21 +1,29 @@
-function anagram(s1, s2) {
-  if (s1.length !== s2.length) {
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
     return false;
   }
-  const counter = {};
 
-  for (let char of s1) {
-    counter[char] = (counter[char] || 0) + 1;
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
   }
+  console.log(lookup);
 
-  for (let char of s2) {
-    counter[char] = (counter[char] || 0) - 1;
-  }
-
-  for (let key in counter) {
-    if (counter[key] !== 0) {
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
       return false;
+    } else {
+      lookup[letter] -= 1;
     }
   }
+
   return true;
 }
+
+// {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+validAnagram("anagrams", "nagaramm");

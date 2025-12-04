5e660669dc0bb08f01c3f1c6a31dfaa37a086a1e
Steve Saju <stevesajustephen@gmail.com>
2025-12-04 17:18:17 +0100

    Sliding window pattern

--- Sliding Window/largeSum.js
+++ Sliding Window/largeSum.js
@@ -19,3 +19,26 @@ function largeSum(nums, n) {
 }
 
 largeSum([1, 2, 3, 4, 5], 2);
+
+// largeSubSetSum with sliding window Pattern
+// [2, 1, 5, 1, 3, 2]
+
+function largeSumWindow(nums, n) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < n; i++) {
+    tempSum = tempSum + nums[i];
+  }
+  maxSum = tempSum;
+
+  for (let i = n; i < nums.length; i++) {
+    tempSum = tempSum - nums[i - n] + nums[i];
+    if (tempSum > maxSum) {
+      maxSum = tempSum;
+    }
+  }
+  console.log(maxSum);
+}
+
+largeSumWindow([2, 1, 5, 1, 3, 2], 3);
